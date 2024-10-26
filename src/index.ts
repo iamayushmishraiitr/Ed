8ff789e27da3594ff02bd18874a235aba271f72a
iamayushmishtaiitr <iamayushmishra1510@gmail.com>
2024-10-26 14:09:07 +0530

    new

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,12 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectTodb from "./DataBase/connecToDB";
-
+import { Request,Response } from "express";
 const app =express() ;
 dotenv.config();
 app.use(cors());
 const PORT = process.env.PORT;
-app.get('/' ,(req,res)=>{
+app.get('/' ,(req:Request,res:Response)=>{
      res.send("hellow ")
 })
 app.listen(PORT, () => {
